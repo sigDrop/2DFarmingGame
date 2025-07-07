e4a35165240ee5c19a95b20edeb3da56247f62a2
sigDrop <sergejilaev533@gmail.com>
2025-07-07 20:24:07 +0500

    Add player animations

--- Assets/Scripts/Player/PlayerContoller.cs
+++ Assets/Scripts/Player/PlayerContoller.cs
@@ -8,15 +8,26 @@ public class PlayerContoller : MonoBehaviour
 
     public InputActionReference moveInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator animator;
+
     void Start()
     {
        
     }
 
-    // Update is called once per frame
     void Update()
     {
         rigidbody.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+        if(rigidbody.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rigidbody.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        animator.SetFloat("speed", rigidbody.linearVelocity.magnitude);
     }
 }
