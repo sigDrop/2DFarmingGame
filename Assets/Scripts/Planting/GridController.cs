60991f21f7a570a9233836eaeb2edd3573f37859
sigDrop <sergejilaev533@gmail.com>
2025-07-14 18:25:15 +0500

    Build And Finish Project

--- Assets/Scripts/Planting/GridController.cs
+++ Assets/Scripts/Planting/GridController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class GridController : MonoBehaviour
--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class UIController : MonoBehaviour
 {
